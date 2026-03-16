241dd88fd90f0b4b823d426fe2e09bb344d32cf9
Gjgjkkirill <shvetcovkirill@gmail.com>
2026-03-16 09:25:21 +0500

    Number6

---
+++ Number6_Dated_06.02.2026.pas
@@ -0,0 +1,15 @@
+﻿program functio;
+var x: real;
+begin
+  write('Введите x: ');
+  readln(x);
+  if x < 0 then
+    writeln(x*x)
+  else
+    begin
+      if x > 0 then
+        writeln(sqrt(x))
+      else
+        writeln(cos(x));
+    end;
+end.
