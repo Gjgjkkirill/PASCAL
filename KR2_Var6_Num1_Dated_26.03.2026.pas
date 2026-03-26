87ba849bb54f08b99b229b4a1cf0cd059cb3a85d
Gjgjkkirill <shvetcovkirill@gmail.com>
2026-03-26 07:56:24 +0500

    KR2_Var6

---
+++ KR2_Var6_Num1_Dated_26.03.2026.pas
@@ -0,0 +1,14 @@
+﻿program var6num1;
+var a,d,c: real;
+begin
+  write('Введите a d c через пробел: ');
+  readln(a,d,c);
+  a:= 2*a - 1;
+  if a <> 0 then
+  begin
+    d:= ArcTan(c - d/2)/a;
+    writeln('Результат формулы: ', d);
+  end
+  else
+    writeln('Произошло деление на ноль!');
+end.
---
+++ KR2_Var6_Num2_Dated_26.03.2026.pas
@@ -0,0 +1,14 @@
+﻿program var6num2;
+var a,d,c: real;
+begin
+  write('Введите a d c через пробел: ');
+  readln(a,d,c);
+  a:= a*a - 1;
+  if a <> 0 then
+  begin
+    d:= (4 * Sin(c) - (d/2) + 23) / a;
+    writeln('Результат формулы: ', d);
+  end
+  else
+    writeln('Произошло деление на ноль!');
+end.
